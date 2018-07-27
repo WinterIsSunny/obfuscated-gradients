340ec891d7ce5bd9116904378bf9aa76522245a7
Yu Su <yusu@campus-015-108.ucdavis.edu>
2018-07-27 14:04:13 -0700

    modified

--- defensegan/defensegan_attack.py
+++ defensegan/defensegan_attack.py
@@ -253,7 +253,7 @@ print("Preds",model.predict(x_test)[0])
 
 res = []
 for i in range(30):
-    pre_adv = attack1.attack_untargeted(np.array([x_test[0]]),y_test[0],iterations = 100)
+    pre_adv = attack1.attack_untargeted(x_test[0],y_test[0],iterations = 100)
     dist = pre_adv - x_test[0]
     res.append(dist)
     
