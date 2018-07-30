f3e6935d1d24d6ce9077348f56da83a2f9066fb0
Yu Su <yusu@campus-034-160.ucdavis.edu>
2018-07-30 15:07:16 -0700

    modified

--- pixeldefend/wrapper.py
+++ pixeldefend/wrapper.py
@@ -36,6 +36,7 @@ class MyModel:
         
         grad, = tf.gradients(self.model.xent, self.model.x_input)
         adv_def = pixeldefend(image)
+        #adv = np.clip(adv, lower, upper)
         g, l, p = sess.run([grad, self.model.xent, self.model.predictions],
                            {self.model.x_input: [adv_def], self.model.y_input: [self.TRUE_CLASS]})
         #print(" prediction of adversarial: ",)
