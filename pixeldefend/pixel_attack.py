a32ff1aed5510eb124085b6692dec7ab0386ad69
Yu Su <yusu@campus-034-160.ucdavis.edu>
2018-07-30 13:29:41 -0700

    modified

---
+++ pixeldefend/pixel_attack.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 12:45:30 2018
+
+@author: yusu
+"""
+
+import tensorflow as tf
+import numpy as np
+from model import Model
+import models.pixelcnn_cifar as pixelcnn
+from utils import *
+from defense import *
+from wrapper import MyModel
+import torch 
+
+
+
+class blackbox:
+    def __init__(self,model):
+        self.model = model
+        
+    def attack_untargeted(self, x0, y0, alpha = 2, beta = 0.005, iterations = 1000):
+        """ Attack the original image and return adversarial example
+            model: (pytorch model)
+            alpha: learning rate 
+            beta: learning rate
+            train_dataset: set of training data
+            (x0, y0): original image
+        """
+        #print(x0.type())
+        #print(y0.type())
+        #print(model.predict(x0).type())
+        #y0 = y0.cuda()
+        y0 = np.array(y0)
+        #print("the type of y0 is : ",type(y0))
+        #print("the value of y0 is: ", y0)
+        #print("pure label is: ",self.model.predict_label(x0))
+        #print("the type of x0 is: ",type(x0))
+        
+        if (self.model.predict(x0) != y0):
+            print("Fail to classify the image. No need to attack.")
+            return x0
+    
+        num_directions = 1000
+        best_theta, g_theta = None, float('inf')
+        query_count = 0
+        
+        #timestart = time.time()
+        for i in range(num_directions):
+            theta = torch.randn(x0.shape).type(torch.FloatTensor)
+            #print(theta.size())
+            initial_lbd = torch.norm(theta)
+            theta = theta/torch.norm(theta)
+            lbd, count = self.fine_grained_binary_search( x0, y0, theta, initial_lbd, g_theta)
+            query_count += count
+            if lbd < g_theta:
+                best_theta, g_theta = theta,lbd
+                print("--------> Found distortion %.4f" % g_theta)
+    
+        #timeend = time.time()
+        #print("==========> Found best distortion %.4f in %.4f seconds using %d queries" % (g_theta, timeend-timestart, query_count))
+    
+        
+        
+        
+        #timestart = time.time()
+        print("the best initialization: ",g_theta)
+        g1 = 1.0
+        theta, g2 = best_theta.clone(), g_theta
+        torch.manual_seed(0)
+        opt_count = 0
+        stopping = 0.01
+        prev_obj = 100000
+        for i in range(iterations):
+            gradient = torch.zeros(theta.size())
+            q = 10
+            min_g1 = float('inf')
+            for _ in range(q):
+                u = torch.randn(theta.size()).type(torch.FloatTensor)
+                u = u/torch.norm(u)
+                ttt = theta+beta * u
+                ttt = ttt/torch.norm(ttt)
+                g1, count = self.fine_grained_binary_search_local( x0, y0, ttt, initial_lbd = g2, tol=beta/500)
+                opt_count += count
+                gradient += (g1-g2)/beta * u
+                if g1 < min_g1:
+                    min_g1 = g1
+                    min_ttt = ttt
+            gradient = 1.0/q * gradient
+    
+            if (i+1)%50 == 0:
+                
+                print("Iteration %3d: g(theta + beta*u) = %.4f g(theta) = %.4f distortion %.4f num_queries %d" % (i+1, g1, g2, torch.norm(g2*theta), opt_count))
+                if g2 > prev_obj-stopping:
+                    break
+                prev_obj = g2
+    
+            min_theta = theta
+            min_g2 = g2
+        
+            for _ in range(15):
+                new_theta = theta - alpha * gradient
+                new_theta = new_theta/torch.norm(new_theta)
+                new_g2, count = self.fine_grained_binary_search_local( x0, y0, new_theta, initial_lbd = min_g2, tol=beta/500)
+                opt_count += count
+                alpha = alpha * 2
+                print("alpha in the first for loop is: ",alpha)
+                if new_g2 < min_g2:
+                    min_theta = new_theta 
+                    min_g2 = new_g2
+                else:
+                    break
+    
+            if min_g2 >= g2:
+                for _ in range(15):
+                    alpha = alpha * 0.95
+                    new_theta = theta - alpha * gradient
+                    new_theta = new_theta/torch.norm(new_theta)
+                    new_g2, count = self.fine_grained_binary_search_local( x0, y0, new_theta, initial_lbd = min_g2, tol=beta/500)
+                    opt_count += count
+                    print("alpha in the second for loop is: ",alpha)
+                    if new_g2 < g2:
+                        min_theta = new_theta 
+                        min_g2 = new_g2
+                        break
+    
+            if min_g2 <= min_g1:
+                theta, g2 = min_theta, min_g2
+            else:
+                theta, g2 = min_ttt, min_g1
+    
+            if g2 < g_theta:
+                best_theta, g_theta = theta.clone(), g2
+            
+            #print(alpha)
+            print("%3d th iteration" % i)
+            print("current alpha:",alpha)
+            if alpha < 1e-4:
+                alpha = 1.0
+                print("Warning: not moving, g2 %lf gtheta %lf" % (g2, g_theta))
+                beta = beta * 0.1
+                if (beta < 0.0005):
+                    break
+    
+        #target = model.predict(x0 + g_theta*best_theta)
+        
+        #timeend = time.time()
+        #print("\nAdversarial Example Found Successfully: distortion %.4f target %d queries %d \nTime: %.4f seconds" % (g_theta, target, query_count + opt_count, timeend-timestart))
+        return x0 + np.array(g_theta*best_theta)
+    def fine_grained_binary_search_local(self, x0, y0, theta, initial_lbd = 1.0, tol=1e-5):
+        nquery = 0
+        lbd = initial_lbd
+         
+        if self.model.predict(x0+np.array(lbd*theta)) == y0:
+            lbd_lo = lbd
+            lbd_hi = lbd*1.01
+            nquery += 1
+            while self.model.predict(x0+np.array(lbd_hi*theta)) == y0:
+                lbd_hi = lbd_hi*1.01
+                nquery += 1
+                if lbd_hi > 20:
+                    return float('inf'), nquery
+        else:
+            lbd_hi = lbd
+            lbd_lo = lbd*0.99
+            nquery += 1
+            while self.model.predict(x0+np.array(lbd_lo*theta)) != y0 :
+                lbd_lo = lbd_lo*0.99
+                nquery += 1
+    
+        while (lbd_hi - lbd_lo) > tol:
+            lbd_mid = (lbd_lo + lbd_hi)/2.0
+            nquery += 1
+            if self.model.predict(x0 + np.array(lbd_mid*theta)) != y0:
+                lbd_hi = lbd_mid
+            else:
+                lbd_lo = lbd_mid
+        return lbd_hi, nquery
+    
+    def fine_grained_binary_search(self, x0, y0, theta, initial_lbd, current_best):
+        nquery = 0
+        if initial_lbd > current_best: 
+            if self.model.predict(x0+ np.array(current_best*theta)) == y0:
+                nquery += 1
+                return float('inf'), nquery
+            lbd = current_best
+        else:
+            lbd = initial_lbd
+        
+        ## original version
+        #lbd = initial_lbd
+        #while model.predict(x0 + lbd*theta) == y0:
+        #    lbd *= 2
+        #    nquery += 1
+        #    if lbd > 100:
+        #        return float('inf'), nquery
+        
+        #num_intervals = 100
+    
+        # lambdas = np.linspace(0.0, lbd, num_intervals)[1:]
+        # lbd_hi = lbd
+        # lbd_hi_index = 0
+        # for i, lbd in enumerate(lambdas):
+        #     nquery += 1
+        #     if model.predict(x0 + lbd*theta) != y0:
+        #         lbd_hi = lbd
+        #         lbd_hi_index = i
+        #         break
+    
+        # lbd_lo = lambdas[lbd_hi_index - 1]
+        lbd_hi = lbd
+        lbd_lo = 0.0
+    
+        while (lbd_hi - lbd_lo) > 1e-5:
+            lbd_mid = (lbd_lo + lbd_hi)/2.0
+            nquery += 1
+            if self.model.predict(x0 + np.array(lbd_mid*theta)) != y0:
+                lbd_hi = lbd_mid
+            else:
+                lbd_lo = lbd_mid
+        return lbd_hi, nquery
+
+
+
+sess = tf.Session()
+
+orig = np.load('ship.npy')
+TRUE_CLASS = 8
+EPSILON = 8.0
+lower = np.clip(orig - EPSILON, 0, 255)
+upper = np.clip(orig + EPSILON, 0, 255)
+
+model = Model(mode='eval')
+
+model = MyModel(model,sess,TRUE_CLASS)
+
---
+++ pixeldefend/wrapper.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 12:29:27 2018
+
+@author: yusu
+"""
+
+#import torch
+#import numpy as np
+import tensorflow as tf
+from defense import *
+from utils import *
+import models.pixelcnn_cifar as pixelcnn
+
+class MyModel:
+    
+    def __init__(self,model,sess,TRUE_CLASS):
+        self.model = model
+        self.sess = sess
+        self.TRUE_CLASS = TRUE_CLASS
+    
+    def predict(self,image):
+        saver = tf.train.Saver()
+        saver.restore(self.sess, tf.train.latest_checkpoint('data/models/naturally_trained'))
+        
+        x = tf.placeholder(tf.float32, (1, 32, 32, 3))
+        _, out = pixelcnn.model(self.sess, x)
+        
+        logits = self.model.pre_softmax
+        probs = tf.nn.softmax(logits)
+        classify = make_classify(self.sess, self.model.x_input, probs)
+        #classify(orig)
+        
+        #grad, = tf.gradients(self.model.xent, self.model.x_input)
+        adv_def = pixeldefend(image)
+        p = sess.run([self.model.predictions],
+                           {model.x_input: [adv_def], model.y_input: [self.TRUE_CLASS]})
+        #print(" prediction of adversarial: ",)
+        return p
+        
