a7fc05911024f51a662088afec0d24026ccc9bfa
WinterIsSunny <yus0601@outlook.com>
2018-08-03 19:51:51 -0700

    modified

--- randomization/attack_randomization.py
+++ randomization/attack_randomization.py
@@ -70,7 +70,7 @@ class blackbox:
         for i in range(iterations):
             
            # print("iteration:",i)
-            if g_theta < 1:
+            if g_theta < 0.001:
                 break
             gradient = torch.zeros(theta.size())
             q = 10
