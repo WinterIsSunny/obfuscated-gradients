4aef538ae56b6867848cf355405393fc4b08c8c2
WinterIsSunny <yus0601@outlook.com>
2018-08-06 13:03:09 -0700

    modified

--- randomization/attack_randomization.py
+++ randomization/attack_randomization.py
@@ -262,11 +262,11 @@ image = orig.copy()/255.0
 #print("Before loading model")
 model = MyModel(inceptionv3,sess,[0.0,255.0])
 
-y0 = model.predict(image)
+#y0 = model.predict(image)
 #print("after loading model")
 attack = blackbox(model)
-#label = 282
-print("predict pure image:",y0 )
+label = 287
+print("predict pure image:",label )
 #print("predict pure image:", y1)
 adv = attack.attack_untargeted(image,y0)
 print("new label:", model.predict(adv))
