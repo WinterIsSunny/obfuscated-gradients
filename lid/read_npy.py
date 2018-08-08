1009d31432ffad467c241372591d9505c90633cf
WinterIsSunny <yus0601@outlook.com>
2018-08-08 16:04:28 -0700

    modified

--- lid/read_npy.py
+++ lid/read_npy.py
@@ -10,4 +10,6 @@ import numpy as np
 
 file_name = os.path.join("data/lid_cifar_cw-l2_20.npy")
 data = np.load(file_name)
+print("shape of this file:", data.shape)
+print("shape of one item:",data[0].shape)
 print("what is saved in this file:", data[0:5])
