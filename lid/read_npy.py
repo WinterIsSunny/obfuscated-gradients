1be17c70e6be4805a0aa0d3ea2253001d4493a1e
WinterIsSunny <yus0601@outlook.com>
2018-08-08 19:09:29 -0700

    modified

--- lid/read_npy.py
+++ lid/read_npy.py
@@ -8,12 +8,11 @@ Created on Wed Aug  8 15:59:45 2018
 import os
 import numpy as np
 
-file_name = os.path.join("data/lid_cifar_cw-l2_20.npy")
+file_name = os.path.join("data/lid_cifar_blackbox.npy")
 data = np.load(file_name)
 print("shape of this file:", data.shape)
 print("shape of one item:",data[0].shape)
-print("what is saved in this file:", data[0:5])
-print("at the end of the file, negative?:",data[-5:-1])
+print("what is saved in this file:", data)
 
 
 
