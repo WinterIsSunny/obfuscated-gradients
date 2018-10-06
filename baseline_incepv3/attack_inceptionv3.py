c29fd6e0f01a7fe457dff6f4dccdc6b793550b42
WinterIsSunny <yus0601@outlook.com>
2018-10-06 02:12:25 -0700

    odified

--- baseline_incepv3/attack_inceptionv3.py
+++ baseline_incepv3/attack_inceptionv3.py
@@ -76,7 +76,7 @@ class blackbox:
         for i in range(iterations):
             
            # print("iteration:",i)
-            if g_theta < 0.0001:
+            if g_theta < 0.05:
                 break
             gradient = torch.zeros(theta.size())
             q = 10
