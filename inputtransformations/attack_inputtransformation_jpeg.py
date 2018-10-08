3c569f40470b30583ddbf48e4e5093f3e201c90d
WinterIsSunny <yus0601@outlook.com>
2018-10-08 07:11:48 -0700

    odified

--- inputtransformations/attack_inputtransformation_jpeg.py
+++ inputtransformations/attack_inputtransformation_jpeg.py
@@ -146,11 +146,11 @@ class blackbox:
 #            print("current alpha:",alpha)
 #            print("g_theta")
 #            print("number of queries:", opt_count+query_count)
-            if alpha < 1e-4:
+            if alpha < 1e-9:
                 alpha = 1.0
                 print("Warning: not moving, g2 %lf gtheta %lf" % (g2, g_theta))
                 beta = beta * 0.1
-                if (beta < 0.0000005):
+                if (beta <  5*1e-10):
                     break
             print("new label in this iteration:", self.model.predict(x0+np.array(best_theta*g_theta)))
             print("=-=-=-=-=-=-=-=-=-=-=-=-will enter next iteration=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
@@ -439,32 +439,60 @@ class blackbox:
 #    
 #    
 sess = tf.Session()
-orig = load_image('cat.jpg')
+#orig = load_image('cat.jpg')
+
+mypath = os.path.join("data/n01440764/")
+print("path of images:", mypath)
+labels = pd.read_csv("data/train.txt", sep = " ", header = None)
+print("type of labels:",type(labels))
+
+files = [load_image(mypath + file) for file in os.listdir(mypath)]
+
+#for file in os.listdir(mypath):
+#    orig = load_image(mypath + file)
+#    files.append(orig)
+    
+images = np.asarray(files[:100])
+images = images/255.0
+
 #print("type of orig:. ", type(orig))
 #print("size of orig: ", orig.shape)
 #print("length of orig: ",len(orig))
 #TARGET = 924 # guacamole  
 
 model = MyModel(inceptionv3,sess,[0.0,255.0])
+attack = blackbox(model)
 #print(orig.shape)
 #image = tf.convert_to_tensor(orig)
 #image_extend = tf.expand_dims(image, axis=0)
 #print("shape of image_extend: ", image_extend.shape)
-image = np.copy(orig)/255.0
+#image = np.copy(orig)/255.0
 
 #print(len(image),type(image))
-true_label = model.predict(image)
-print("true label of the original image is: ", true_label)
-attack = blackbox(model)
+#true_label = model.predict(image)
+#print("true label of the original image is: ", true_label)
+
+
 adv = attack.attack_untargeted(image,true_label, alpha = 4, beta = 0.05, iterations = 1000)
 
 #adv = attack.attack_targeted(image,true_label,924)
 
-adv_label = model.predict(adv)#print("target lable is: ", TARGET)
-print("label after attack is: ", adv_label)
-
-
-
+dist = []
+count = []
+label_tmp = np.zeros(15)
+for i in range(15):
+    print("================attacking image ",i+1,"=======================")
+    adv,queries = attack.attack_untargeted(images[i],label_tmp[i],alpha = 2*1e-6, beta = 5*1e-8, iterations = 1000)
+    dist.append(np.linalg.norm(adv-images[i]))
+    count.append(queries)
+    
+print("the distortions for 15 images :")
+for i in dist:
+    print(i)
+print("the number of queries for 15 images :")
+for j in count:
+    print(j)
+    
 #classify = make_classify(sess, x, probs)
 #classify(orig, target_class=TARGET)
 
