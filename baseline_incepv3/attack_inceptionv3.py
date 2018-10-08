f72d9cc4f636bfe6037699da488760340358af62
WinterIsSunny <yus0601@outlook.com>
2018-10-08 08:20:29 -0700

    ojdified

--- baseline_incepv3/attack_inceptionv3.py
+++ baseline_incepv3/attack_inceptionv3.py
@@ -156,7 +156,7 @@ class blackbox:
                 alpha = 0.01
                 print("Warning: not moving, g2 %lf gtheta %lf" % (g2, g_theta))
                 beta = beta * 0.1
-                if (beta < 5*1e-10):
+                if (beta < 1e-12):
                     print("beta is too samll")
                     break
             print("distortion in this iteration:", g_theta)
--- thermometer/attack_thermometer.py
+++ thermometer/attack_thermometer.py
@@ -124,7 +124,7 @@ class blackbox:
                 new_g2, count = self.fine_grained_binary_search_local( x0, y0, new_theta, initial_lbd = min_g2, tol=beta/50)
                 opt_count += count
                 alpha = alpha * 2
-#                print("alpha in the first for loop is: ",alpha)
+                print("alpha in the first for loop is: ",alpha)
                 if new_g2 < min_g2:
                     min_theta = new_theta 
                     min_g2 = new_g2
@@ -139,7 +139,7 @@ class blackbox:
                     new_theta = new_theta/torch.norm(new_theta)
                     new_g2, count = self.fine_grained_binary_search_local( x0, y0, new_theta, initial_lbd = min_g2, tol=beta/50)
                     opt_count += count
-#                    print("alpha in the second for loop is: ",alpha)
+                    print("alpha in the second for loop is: ",alpha)
                     if new_g2 < g2:
                         min_theta = new_theta 
                         min_g2 = new_g2
@@ -280,7 +280,7 @@ dist = []
 count = []
 for i in range(15):
     print("&*&*&*&*&*&*&*&* this is image ",i,"&*&*&*&*&*&**&")
-    mod,queries = attack.attack_untargeted(new_img[i],label[i],alpha = 2, beta = 0.0005, iterations = 1000)
+    mod,queries = attack.attack_untargeted(new_img[i],label[i],alpha = 2, beta = 0.005, iterations = 1000)
     dist.append(np.linalg.norm(mod))
     count.append(queries)
     
