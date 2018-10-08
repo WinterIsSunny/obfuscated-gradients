66bbb63a1b0b96b9ef8c3537edce368566cc8477
WinterIsSunny <yus0601@outlook.com>
2018-10-08 07:59:22 -0700

    ojdified

--- baseline_mnist/attack_mnist.py
+++ baseline_mnist/attack_mnist.py
@@ -162,7 +162,7 @@ class blackbox:
                 alpha = 1.0
                 print("Warning: not moving, g2 %lf gtheta %lf" % (g2, g_theta))
                 beta = beta * 0.1
-                if (beta < 0.0005):
+                if (beta < 1e-5):
                     print("beta is too samll")
                     break
             print("distortion in this iteration:", g_theta)
