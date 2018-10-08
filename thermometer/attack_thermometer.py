69c6b696dc1b4e4d32e35a776d837f02a07dadbd
WinterIsSunny <yus0601@outlook.com>
2018-10-08 08:31:51 -0700

    ojdified

--- thermometer/attack_thermometer.py
+++ thermometer/attack_thermometer.py
@@ -155,9 +155,9 @@ class blackbox:
             
             #print(alpha)
 #            print("%3d th iteration" % i)
-#            print("current alpha:",alpha)
+            print("current alpha:",alpha)
 #            print("g_theta")
-#            print("number of queries:", opt_count+query_count)
+            print("number of queries:", opt_count+query_count)
             if alpha < 1e-4:
                 alpha = 1.0
                 print("Warning: not moving, g2 %lf gtheta %lf" % (g2, g_theta))
