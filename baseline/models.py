9605fa19fb8d46f3b5b3c5f732a050e3d978404c
WinterIsSunny <yus0601@outlook.com>
2018-10-31 15:48:19 -0700

    change cifar-baseline

--- baseline/models.py
+++ baseline/models.py
@@ -28,7 +28,7 @@ class PyModel(object):
 
         new_img = [new_img]
         label = self.sess.run(self.model.predictions, {self.model.x_input: new_img})
-        print("predicted label:", label)
+#        print("predicted label:", label)
 
         return label
         
