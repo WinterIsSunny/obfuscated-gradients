017166211c39be31212d319937f24c22208a53e2
WinterIsSunny <yus0601@outlook.com>
2018-11-03 17:11:02 -0700

    change base

--- baseline_incepv3/attack_inceptionv3.py
+++ baseline_incepv3/attack_inceptionv3.py
@@ -266,6 +266,7 @@ sess = tf.Session()
 
 # load images and lables
 images,labels = read_images("/data3/ILSVRC2012/train/",110)
+images = np.asarray(images)
 images = images/255
 
 #orig = load_image('cat.jpg')
