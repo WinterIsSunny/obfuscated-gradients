81d01da3341ddd5c9c4ddad6f0b5507dfd19b68f
WinterIsSunny <yus0601@outlook.com>
2018-11-15 20:08:57 -0800

    change input

--- baseline_incepv3/attack_inceptionv3.py
+++ baseline_incepv3/attack_inceptionv3.py
@@ -40,7 +40,7 @@ class blackbox:
             print("Fail to classify the image. No need to attack.")
             return x0,0
     
-        num_directions = 1000
+        num_directions = 5000
         best_theta, g_theta = None, float('inf')
         query_count = 0
         
